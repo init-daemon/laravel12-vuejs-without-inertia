5dbb2a10d5d31b45b9b04c50a997caa04a74d238
Rxon <eric100son@gmail.com>
2026-04-24 22:19:35 +0300

    fix(eslint): remove TypeScript config and fix flat config setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,24 @@
-import prettier from 'eslint-config-prettier';
-import vue from 'eslint-plugin-vue';
+import prettier from 'eslint-config-prettier'
+import vue from 'eslint-plugin-vue'
 
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+export default [
+    ...vue.configs['flat/essential'],
 
-export default defineConfigWithVueTs(
-    vue.configs['flat/essential'],
-    vueTsConfigs.recommended,
     {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'resources/js/components/ui/*'],
+        ignores: [
+            'vendor',
+            'node_modules',
+            'public',
+            'bootstrap/ssr',
+            'tailwind.config.js',
+            'resources/js/components/ui/*',
+        ],
     },
     {
         rules: {
             'vue/multi-word-component-names': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
         },
     },
+
     prettier,
-);
+]
